14be8b081b7075107f5d40b22ad994ddf7419c8d
Gabriel Ramalho <guimaraes.gabriell@outlook.com>
2023-06-22 18:10:25 -0300

    feat: Página das categorias agora mostram seus respectivos produtos

--- views/category.php
+++ views/category.php
@@ -43,16 +43,9 @@
                             <span aria-hidden="true">«</span>
                             </a>
                         </li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li>
-                            <a href="#" aria-label="Next">
-                            <span aria-hidden="true">»</span>
-                            </a>
-                        </li>
+                        <?php foreach($pages as $page): ?>
+                        <li><a href="<?php echo $page['link'] ?>"><?= $page['page']?></a></li>
+                        <?php endforeach; ?>
                         </ul>
                     </nav>                        
                 </div>
